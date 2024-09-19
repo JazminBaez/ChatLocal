03672b637d29d30eb179ccd301c20be0b6f0666d
JazminBaez <baezdjazmin@email.com>
2024-09-19 14:57:29 -0300

    configura gitignore

---
+++ Server/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public class Server
+    {
+        IPHostEntry host;
+        IPAddress ipAdd;
+        IPEndPoint endPoint;
+
+        Socket s_socket;
+        public Server(string ip, int port)
+        {
+            host = Dns.GetHostByName(ip);
+            ipAdd = host.AddressList[0];
+            endPoint = new IPEndPoint(ipAdd, port);
+
+            s_socket = new Socket(ipAdd.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            s_socket.Bind(endPoint);
+            s_socket.Listen(10);
+
+        }
+    }
+}
