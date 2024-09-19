01bbccab2a2d60cc1d0d2a4e639078d13d54373c
JazminBaez <baezdjazmin@email.com>
2024-09-19 18:38:42 -0300

    crea cliente

---
+++ Client/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Client
+    {
+        IPHostEntry host;
+        IPAddress ipAdd;
+        IPEndPoint endPoint;
+
+        Socket c_socket;
+        public Client(string ip, int port) {
+            host = Dns.GetHostEntry(ip);
+            ipAdd = host.AddressList[0];
+            endPoint = new IPEndPoint(ipAdd, port);
+
+            c_socket = new Socket(ipAdd.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            c_socket.Bind(endPoint);
+        }
+
+    }
+}
