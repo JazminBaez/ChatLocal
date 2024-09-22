028c6bbbcd3587d167dcbd49dd6b09a8271d6bf3
JazminBaez <baezdjazmin@email.com>
2024-09-21 23:10:01 -0300

    crea interfaz de usuario (cliente)

---
+++ ChatLocalCliente/JSONSerialization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Serialization
+{
+    class JSONSerialization
+    {
+        public static byte[] Serialize(object toSerialize)
+        {
+            string jsonString = JsonSerializer.Serialize(toSerialize);
+            return System.Text.Encoding.UTF8.GetBytes(jsonString);
+        }
+
+        public static object Deserialize(byte[] toDeserialize, Type objectType)
+        {
+            string jsonString = System.Text.Encoding.UTF8.GetString(toDeserialize);
+            return JsonSerializer.Deserialize(jsonString, objectType);
+        }
+    }
+}
+
